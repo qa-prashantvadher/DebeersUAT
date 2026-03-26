c1502cd3917b45194c2f3fc000500448bd432f29
PrashantVadher <prashant_vadher@epam.com>
2026-03-26 16:02:23 +0530

    Env changes in the conftest.py file

--- DebeersUSUAT/pages/checkout_delivery_page.py
+++ DebeersUSUAT/pages/checkout_delivery_page.py
@@ -13,6 +13,8 @@ class Checkout_Delivery(BasePage):
     premium_city_input = "//input[@id='shippingAddressCity']"
     premium_postal_code_input = "//input[@id='shippingZipCode']"
 
+    premium_add_new_address_option = "//a[@class='anchor btn-add-new js-btn-add-new ']"
+
     premium_address_text = "1128 Park Ave Apt 123"
     premium_city_text = "New York"
     premium_state_text = "NY"
@@ -101,6 +103,69 @@ class Checkout_Delivery(BasePage):
         except:
             print("*****[CHECKOUT-PREMIUM] USER IS NOT REDIRECTED TO PAYMENT PAGE..*****")
 
+    def test_client_service_tax_error_popup_as_guest(self):
+        try:
+            self.timeout(2000)
+            self.click(Checkout_Delivery.premium_delivery_tab)
+            self.timeout(2000)
+            self.select_option(self.delivery_title_dropdown,self.delivery_title_value)
+            self.fill(self.first_name_input,self.first_name_text)
+            self.fill(self.last_name_input,self.last_name_text)
+            self.fill(self.phone_input,self.phone_text)
+            self.fill(self.premium_address_input,self.premium_address_text)
+            #State dropdown
+            self.select_state_dropdown_value(self.premium_state_dropdown, self.premium_state_text)
+            self.fill(self.premium_city_input,"TESTING")
+            self.fill(self.premium_postal_code_input, "TESTING")
+            if self.is_checked(self.gift_checkbox):
+                print("GIFT OPTION IS ALREADY SELECTED...")
+            else:
+                self.click(self.gift_checkbox)
+            self.timeout(1000)
+            self.fill(self.gift_message_input, self.gift_message_guest_premium_text)
+            self.timeout(2000)
+        except:
+            print("*****[CHECKOUT-PREMIUM] NOT ABLE TO ENTER DELIVERY ADDRESS INFO..*****")
+
+
+    def test_client_service_tax_error_popup_as_register(self):
+        try:
+            self.timeout(2000)
+            self.click(Checkout_Delivery.premium_delivery_tab)
+            self.timeout(2000)
+            self.select_option(self.delivery_title_dropdown,self.delivery_title_value)
+
+            if self.is_checked(self.gift_checkbox):
+                print("GIFT OPTION IS ALREADY SELECTED...")
+            else:
+                self.click(self.gift_checkbox)
+            self.timeout(1000)
+            self.fill(self.gift_message_input, self.gift_message_registered_premium_text)
+            self.timeout(2000)
+            delivery_date = self.get_text(self.premium_delivery_date).strip()
+            self.screenshot.take_page_screenshot("CHECKOUT_REGISTERED_PREMIUM")
+            print(f"[CHECKOUT-PREMIUM] DELIVERY DATE: {delivery_date}")
+        except:
+            print("*****[CHECKOUT-PREMIUM] NOT ABLE TO ENTER DELIVERY ADDRESS INFO..*****")
+
+    def test_add_new_address_as_register(self):
+        try:
+            self.timeout(2000)
+            self.click(self.premium_add_new_address_option)
+            self.timeout(2000)
+            print("*****[CHECKOUT-PREMIUM] NEW ADDRESS SECTION IS NOW VISIBLE..*****")
+        except:
+            print("*****[CHECKOUT-PREMIUM] NOT ABLE TO OPEN ADD NEW ADDRESS SECTION..*****")
+
+    def test_enter_invalid_new_delivery_address(self):
+        try:
+            self.fill(self.premium_address_input, self.premium_address_text)
+            # State dropdown
+            self.select_state_dropdown_value(self.premium_state_dropdown, self.premium_state_text)
+            self.fill(self.premium_city_input, "TESTING")
+            self.fill(self.premium_postal_code_input, "TESTING")
+        except:
+            print("*****[CHECKOUT-PREMIUM] NOT ABLE TO ENTER NEW INVALID ADDRESS..*****")
 
     def test_self_collect_in_store_as_guest(self):
         try:
