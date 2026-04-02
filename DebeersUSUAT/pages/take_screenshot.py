638e2718d7de4646db60b1ca2e9e0c72d181dea2
PrashantVadher <prashant_vadher@epam.com>
2026-04-02 16:22:12 +0530

    fixed sub folder error

--- DebeersUSUAT/pages/take_screenshot.py
+++ DebeersUSUAT/pages/take_screenshot.py
@@ -1,12 +1,20 @@
 import time
 import os
 from pages.base_page import BasePage
+from dotenv import load_dotenv
+load_dotenv(override=True)
 
+env = os.getenv("ENVIRONMENT")
 
 
 class PageScreenshot(BasePage):
     date_folder = time.strftime('%d%m%Y')
-    base_path = r"D:\Python Code\Screenshots\DB-UAT"
+
+    if env == "UAT":
+        base_path = r"D:\Python Code\Screenshots\DB-UAT\US"
+    else:
+        base_path = r"D:\Python Code\Screenshots\DB-PROD\US"
+
     order_sub_folder = "ORDERS"
     other_sub_folder = "OTHERS"
 
@@ -17,7 +25,7 @@ class PageScreenshot(BasePage):
 
     def take_page_screenshot(self, keyword):
         timestamp = time.strftime('%d-%m-%Y_%H-%M')
-        other_screenshot_full_path = os.path.join(self.base_path, self.order_sub_folder, self.date_folder)
+        other_screenshot_full_path = os.path.join(self.base_path, self.other_sub_folder, self.date_folder)
         os.makedirs(other_screenshot_full_path, exist_ok=True)
         filename = os.path.join(other_screenshot_full_path, f'{keyword}_{timestamp}.png')
         self.page.screenshot(path=filename)
